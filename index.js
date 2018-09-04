f955062bc4769275e283214a84029fd5a3d17d43
simpart <simpart>
2018-09-04 23:43:51 +0000

    mod size

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
  * @file mofron-comp-form/index.js
  * @author simpart
  */
-let mf = require('mofron');
-let Button = require('mofron-comp-button');
-let Message = require('mofron-comp-message');
-let Margin  = require('mofron-layout-margin');
-let Center  = require('mofron-layout-hrzcenter');
+const mf      = require('mofron');
+const Button  = require('mofron-comp-button');
+const Message = require('mofron-comp-message');
+const HrzPos  = require('mofron-effect-hrzpos');
 
 /**
  * @class Form
@@ -17,7 +16,6 @@ mf.comp.Form = class extends mf.Component {
         try {
             super();
             this.name('Form');
-            this.sizeType('rem');
             this.prmMap('uri', 'child');
             this.prmOpt(po, p2);
         } catch (e) {
@@ -26,21 +24,29 @@ mf.comp.Form = class extends mf.Component {
         }
     }
     
-    initDomConts (mgn, cnt) {
+    initDomConts () {
         try {
             super.initDomConts();
-            this.target().style({ 'width' : '100%' });
+            let conts  = new mf.Component({});
+            let submit = new mf.Component({
+                width : '100%'
+            });
             
-            this.layout([
-                new Margin('top', (undefined === mgn) ? 0.25 : mgn),
-                new Center((undefined === cnt) ? 70 : cnt)
-            ]);
+            this.child([ conts, submit ]);
+            this.target(conts.target());
+            this.child([this.message()]);
             
-            super.addChild(this.message(), false);
-            let sub = this.submitComp();
-            super.addChild(sub.parent().parent());
-            
-            this.initKeyEvent();
+            this.submitTgt(submit.target());
+            this.switchTgt(
+                submit.target(),
+                (p1) => {
+                    try { p1.addChild(p1.submitComp()); } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+                    }
+                }
+            );
+            //this.initKeyEvent();
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -49,32 +55,32 @@ mf.comp.Form = class extends mf.Component {
     
     initKeyEvent () {
         try {
-            if (undefined !== window.onkeyup) {
-                let form = this;
-                window.onkeyup = (e) => {
-                    try {
-                        let key      = e.keyCode ? e.keyCode : e.which;
-                        let chd      = form.child();
-                        let send_ret = null;
-                        for (let cidx in chd) {
-                            if (true !== mf.func.isInclude(chd[cidx], 'Form')) {
-                                continue;
-                            }
-                            if ( (13 === key) &&
-                                 (true === chd[cidx].isFocused())) {
-                                send_ret = form.send();
-                                if (null !== send_ret) {
-                                    form.message(send_ret['cause']);
-                                }
-                                break;
-                            }
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                }
-            }
+//            if (undefined !== window.onkeyup) {
+//                let form = this;
+//                window.onkeyup = (e) => {
+//                    try {
+//                        let key      = e.keyCode ? e.keyCode : e.which;
+//                        let chd      = form.child();
+//                        let send_ret = null;
+//                        for (let cidx in chd) {
+//                            if (true !== mf.func.isInclude(chd[cidx], 'Form')) {
+//                                continue;
+//                            }
+//                            if ( (13 === key) &&
+//                                 (true === chd[cidx].isFocused())) {
+//                                send_ret = form.send();
+//                                if (null !== send_ret) {
+//                                    form.message(send_ret['cause']);
+//                                }
+//                                break;
+//                            }
+//                        }
+//                    } catch (e) {
+//                        console.error(e.stack);
+//                        throw e;
+//                    }
+//                }
+//            }
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -126,7 +132,7 @@ mf.comp.Form = class extends mf.Component {
         try {
             if (undefined === func) {
                 /* getter */
-                return (undefined === this.m_sendevt) ? null : this.m_sendevt;
+                return (undefined === this.m_sendevt) ? [] : this.m_sendevt;
             }
             /* setter */
             if ('function' !== typeof func) {
@@ -144,54 +150,42 @@ mf.comp.Form = class extends mf.Component {
     
     send () {
         try {
-           let ret_chk = this.checkValue();
-           if (null !== ret_chk) {
-               return ret_chk;
-           }
-           
-           let cb   = this.callback();
-           let xhr  = new XMLHttpRequest();
-           let form = this;
-           xhr.addEventListener('load', function(event) {
-               if (null != cb) {
-                   let cb_ret = null;
-                   for (let cidx in cb) {
-                       cb_ret = cb[cidx][0](JSON.parse(this.response), form, cb[cidx][1]);
-                       if (null !== cb_ret) {
-                           form.message(cb_ret);
-                       }
-                   }
-               }
-           });
-           var send_uri = (undefined === this.uri()) ? this.m_param : this.uri();
-           if (null === send_uri) {
-               throw new Error('could not find uri');
-           }
-           xhr.open('POST', send_uri);
-           let send_val = this.value();
-           let optprm = this.optionParam();
-           if (null !== optprm) {
-               for (let oidx in optprm) {
-                   send_val[oidx] = optprm[oidx];
-               }
-           }
-           
-           let snd_evt = this.sendEvent();
-           if (null !== snd_evt) {
-               let ev_ret = null;
-               for (let eidx in snd_evt) {
-                   ev_ret = snd_evt[eidx][0](this, snd_evt[eidx][1]);
-                   if (null !== ev_ret) {
-                       return {
-                           index : -1,
-                           cause : ev_ret
-                       };
-                   }
-               }
-           }
-           
-           xhr.send(JSON.stringify(send_val));
-           return null;
+            this.checkValue();
+            if (true === this.message().visible()) {
+                return;
+            }
+            if (null === this.uri()) {
+                throw new Error('could not find uri');
+            }
+            
+            let xhr  = new XMLHttpRequest();
+            let form = this;
+            xhr.addEventListener(
+                'load',
+                (event) => {
+                    try {
+                        if (null !== form.callback()) {
+                            form.callback()[0](form, form.callback()[1]);
+                        }
+                    } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+                    }
+                }
+            );
+            
+            xhr.open('POST', this.uri());
+            let snd_evt = this.sendEvent();
+            let ev_ret  = null;
+            for (let eidx in snd_evt) {
+                ev_ret = snd_evt[eidx][0](this, snd_evt[eidx][1]);
+                if (null !== ev_ret) {
+                    this.msgText(ev_ret);
+                    return;
+                }
+            }
+            
+            xhr.send(JSON.stringify(this.value()));
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -217,28 +211,22 @@ mf.comp.Form = class extends mf.Component {
     
     checkValue () {
         try {
-            let items    = this.getItems();
-            let ret_chk  = null;
-            let form_idx = 0;
+            let items = this.getItems();
             if (null === items) {
-               return {
-                   index : -1,
-                   cause : 'form is no element'
-               };
+                this.msgText('internal error');
+                return;
             }
+            /* check form item value */
+            let ret_chk = null;
             for (let idx in items) {
-                /* null check */
                 ret_chk = items[idx].checkValue();
                 if (null !== ret_chk) {
-                    return {
-                        index : idx,
-                        cause : ret_chk
-                    }
+                    this.msgText(ret_chk);
+                    return;
                 }
             }
             /* reset message */
-            this.message(null);
-            return null;
+            this.msgText(null);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -258,6 +246,11 @@ mf.comp.Form = class extends mf.Component {
                 val_nm = items[idx].sendKey();
                 ret_val[(null === val_nm)? 'prm_' + idx : val_nm] = items[idx].value();
             }
+            /* get option param */
+            let optprm = this.optionParam();
+            for (let oidx in optprm) {
+                ret_val[oidx] = optprm[oidx];
+            }
             return ret_val;
         } catch (e) {
             console.error(e.stack);
@@ -265,13 +258,15 @@ mf.comp.Form = class extends mf.Component {
         }
     }
     
-    message (msg) {
+    message (prm) {
         try {
-            if (undefined === msg) {
+            if (undefined === prm) {
                 /* getter */
                 if (undefined === this.m_message) {
                     this.message(
                         new Message({
+                            effect  : [ new HrzPos('center') ],
+                            width   : '70%',
                             text    : '',
                             color   : new mf.Color(200,60,60),
                             visible : false
@@ -281,25 +276,28 @@ mf.comp.Form = class extends mf.Component {
                 return this.m_message;
             }
             /* setter */
-            if (true === mf.func.isInclude(msg, 'Message')) {
-                this.m_message = msg;
-            } else if ('string' === typeof msg) {
-                this.message().execOption({
-                    text    : msg,
-                    visible : true
-                });
-                let mevt = this.msgEvent();
-                if (null !== mevt[0]) {
-                    mevt[0](msg, mevt[1]);
-                }
-            } else if (null === msg) {
-                this.message().execOption({ 
-                    visible : true
-                });
-                let mevt = this.msgEvent();
-                if (null !== mevt[0]) {
-                    mevt[0](msg, mevt[1]);
-                }               
+            if (true !== mf.func.isInclude(prm, 'Message')) {
+                throw new Error('invalid parameter');
+            }
+            this.m_message = prm;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    msgText (prm) {
+        try {
+            if (undefined === prm) {
+                /* getter */
+                return this.message().text();
+            }
+            /* setter */
+            if (null === prm) {
+                this.message().visible(false);
+            } else if ('string' === typeof prm) {
+                this.message().visible(true);
+                this.message().text(prm);
             } else {
                 throw new Error('invalid parameter');
             }
@@ -326,84 +324,53 @@ mf.comp.Form = class extends mf.Component {
         }
     }
     
-    submitComp (sub) {
+    submitTgt (prm) {
         try {
-            if (undefined === sub) {
+            if (undefined === prm) {
                 /* getter */
-                if (undefined === this.m_submit) {
-                    this.submitComp(
-                        new Button({
-                            text  : 'Send',
-                            size  : new mf.Param(1, 0.3)
-                        })
-                    );
-                }
-                return this.m_submit;
+                return (undefined === this.m_sbmtgt) ? null : this.m_sbmtgt;
             }
             /* setter */
-            let clk_fnc = (tgt, frm) => {
-                try {
-                    let ret = frm.send();
-                    if (null !== ret) {
-                        frm.message(ret['cause']);
-                    }
-                } catch (e) {
-                    console.error(e.stack);
-                    throw e;
-                }
-            };
-            if (true === mf.func.isInclude(sub, 'Button')) {
-                if (undefined !== this.m_submit) {
-                    sub.color(this.m_submit.color());
-                    sub.size(this.m_submit.size()[0], this.m_submit.size()[1]);
-                    sub.clickEvent(clk_fnc, this);
-                    this.m_submit.parent().updChild(this.m_submit, sub);
-                    return;
-                }
-                new mf.Component({
-                    addChild : new mf.Component({
-                        addChild : sub,
-                        style    : {
-                            'position'    : 'relative',
-                            'margin-left' : '100%'    ,
-                            'left'        : '-' + sub.width()
-                        }
-                    })
-                });
-                sub.width((null === sub.width()) ? 1 : undefined);
-                sub.clickEvent(clk_fnc, this);
-                this.m_submit = sub;
-            } else if ('string' === typeof sub) {
-                this.submitComp().execOption({
-                    text : sub
-                });
-            } else {
-                throw new Error('invalid parameter');
+            if (true !== mf.func.isInclude(prm, 'Dom')) {
+                throw new Error('invalid paramter');
             }
+            this.m_sbmtgt = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    submitText (prm) {
-        if (undefined === prm) {
-            /* getter */
-            return this.submitComp().text();
-        }
-        /* setter */
-        this.submitComp().execOption({
-            text : prm
-        });
-    }
-    
-    addChild (chd, idx, flg) {
+    submitComp (prm) {
         try {
-            if (false !== flg) {
-                super.addChild(chd, (undefined === idx) ? this.child().length-1 : idx);
-            } else {
-                super.addChild(chd, idx);
+            if (undefined === prm) {
+                /* getter */
+                if (undefined === this.m_submit) {
+                    this.submitComp(
+                        new Button({
+                            effect     : [ new HrzPos('center') ],
+                            text       : 'Send',
+                            size       : new mf.Param('1rem', '0.3rem'),
+                            sizeValue  : new mf.Param('margin-top', '0.1rem'),
+                            clickEvent : new mf.Param(
+                                (sub, form) => {
+                                    try { form.send(); } catch (e) {
+                                        console.error(e.stack);
+                                        throw e;
+                                    } 
+                                },
+                                this
+                            )
+                        })
+                    );
+                }
+                return this.m_submit;
             }
+            /* setter */
+            if (true !== mf.func.isInclude(prm, 'Component')) {
+                throw new Error('invalid parameter');
+            }
+            this.m_submit = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -412,69 +379,11 @@ mf.comp.Form = class extends mf.Component {
     
     width (prm) {
         try {
-            let hcnt = this.getConfig('layout', 'HrzCenter');
-            if (undefined === prm) {
-                /* getter */
-                if (null === hcnt) {
-                    return null;
-                }
-                return hcnt.rate() + '%';
-            }
-            /* setter */
-            if (null === hcnt) {
-                let chd = this.child();
-                for (let cidx in chd) {
-                    if (true === mf.func.isInclude(chd[cidx], 'Form')) {
-                        chd[cidx].width(prm);
-                    }
-                }
-            } else {
-                hcnt.rate(prm);
-            }
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    height (prm) {
-        try {
-            let mrg = this.getConfig('layout', 'Margin');
-            let chd = this.child();
-            if (undefined === prm) {
-                /* getter */
-                let hret = 0;
-                let mval = (null === mrg) ? 0 : mrg.value();
-                for (let cidx in chd) {
-                    if ( (true === mf.func.isInclude(chd[cidx], 'Message')) &&
-                         (false === chd[cidx].visible())) {
-                        continue;
-                    }
-                    if (parseInt(cidx)+1 == chd.length) {
-                        hret = mf.func.sizeSum(hret, this.submitComp().height());
-                    } else {
-                        hret = mf.func.sizeSum(
-                            hret,
-                            chd[cidx].height(),
-                            (true === mf.func.isInclude(chd[cidx], 'Input')) ? 0.15 : 0
-                        );
-                    }
-                    hret = mf.func.sizeSum(hret, mval);
-                }
-                hret = mf.func.sizeSum(hret, mval);
-                return hret;
-            }
-            /* setter */
-            if (null === mrg) {
-                if ('number' !== typeof prm) {
-                    throw new Error('invalid paramter');
-                }
-                let chei = prm / (0 === chd.length) ? 1 : chd.length;
-                for (let cidx in chd) {
-                    chd[cidx].height(chei);
-                }
-            } else {
-                mrg.value(prm);
+            let ret = super.width(prm);
+            if (undefined === ret) {
+                this.adom().child()[0].style({
+                    width : prm
+                });
             }
         } catch (e) {
             console.error(e.stack);
@@ -514,7 +423,7 @@ mf.comp.Form = class extends mf.Component {
     optionParam (prm) {
         if (undefined === prm) {
             /* getter */
-            return (undefined === this.m_optprm) ? null : this.m_optprm;
+            return (undefined === this.m_optprm) ? [] : this.m_optprm;
         }
         if ('object' !== typeof prm) {
             throw new Error('invalid parameter');
@@ -522,5 +431,5 @@ mf.comp.Form = class extends mf.Component {
         this.m_optprm = prm;
     }
 }
-module.exports = mofron.comp.Form;
+module.exports = mf.comp.Form;
 /* end of file */
